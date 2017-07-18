c07bd7e1059ad64f2db1804e4e65fa2314c21c14
quheng <494862190@qq.com>
2017-07-18 17:11:17 +0800

    update api info

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# variable
+@app.route("/id/<id>")
+def variable(id=None):
+    return id
+
+# key-value
+@app.route("/key-value")
+def key_value():
+    return jsonify(request.args)
+
+# form-data can not use in GET method
+@app.route('/form-data', methods=['POST'])  #default methods is GET
+def form_data():
+    return jsonify(request.form)
